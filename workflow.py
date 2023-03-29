476e1dd8b252fd6a7e0727d02e83c1a669d5a515
gdfishhannah <gdfishhannah@gmail.com>
2023-03-29 18:22:29 +0800

    change docker path

--- workflow.py
+++ workflow.py
@@ -10,6 +10,7 @@ def train_options():
     parser.add_argument("--project_id", help='project id', )
     parser.add_argument("--obs_bucket_name", help='obs bucket name')
     parser.add_argument("--project_name", help='project name')
+    parser.add_argument("--image_url")
     args = parser.parse_args()
     return args
 
@@ -37,7 +38,7 @@ job_step = wf.steps.JobStep(
     algorithm=wf.BaseAlgorithm(
         code_dir="obs://{}/{}/".format(opt.obs_bucket_name, opt.project_name),
         command='python ${{MA_JOB_DIR}}/{}/training.py'.format(opt.project_name),
-        engine=wf.steps.JobEngine(image_url="hwstaff_maexp/torch1.8-dxh:v001"),
+        engine=wf.steps.JobEngine(image_url=opt.image_url),
     ),
     inputs=[wf.steps.JobInput(name="data_url",
                               data=wf.data.OBSPlaceholder(name="dataset_input", object_type="directory",
